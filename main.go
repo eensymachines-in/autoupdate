c2e542bd8f694020dd65a088a13cf5cbdb1f0925
Niranjan <kneerunjun@gmail.com>
2024-03-22 12:36:12 +0530

    added CORS

--- main.go
+++ main.go
@@ -9,11 +9,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORS(c *gin.Context) {
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+		c.Next()
+	} else {
+		// Everytime we receive an OPTIONS request,
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
+
 func main() {
 	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
 	api := r.Group("/api")
-	api.POST("/test", func(c *gin.Context) {
+	api.Use(CORS).POST("/test", func(c *gin.Context) {
 		fmt.Println("We have received hook notification..")
 		byt, err := io.ReadAll(c.Request.Body)
 		if err != nil {
